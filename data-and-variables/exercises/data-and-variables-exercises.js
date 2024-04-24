22ca7c7a8f1ac9660694d86d3617ad6215d10860
unknown <melcusumano@gmail.com>
2024-04-24 17:06:36 -0500

    Chapter 4 Exercises Complete (Mars/Moon)

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,37 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let spaceShuttle = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceMarsKm = 225000000;
+let distanceMoonKm = 384400;
+const milesPerKilometer = 0.621;
+
+console.log(typeof spaceShuttle);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceMarsKm);
+console.log(typeof distanceMoonKm);
+console.log(typeof milesPerKilometer);
+
+let milesToMars = distanceMarsKm * milesPerKilometer;
+
+console.log(milesToMars)
+
+let hoursToMars = milesToMars / shuttleSpeedMph;
+
+console.log(hoursToMars)
+
+let daysToMars = hoursToMars / 24;
+
+console.log(daysToMars)
+
+console.log(spaceShuttle + " will take " + daysToMars + " days to reach Mars.");
+
+let milesToMoon = distanceMoonKm * milesPerKilometer;
+
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+
+let daysToMoon = hoursToMoon / 24;
+
+console.log(spaceShuttle + " will take " + daysToMoon + " days to reach the Moon.");
