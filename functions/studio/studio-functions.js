2bb94fc42e931feea1aed2fed43fd277686dadb1
unknown <melcusumano@gmail.com>
2024-05-23 22:44:10 -0500

    Functions Studio Complete

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -3,10 +3,10 @@
 // Part One: Reverse Characters
 
 
-// function reverseCharacters(str) {
-//     return str.split("").reverse().join("");
-// }
-// let myString = "apple";
+function reverseCharacters(str) {
+    return str.split("").reverse().join("");
+}
+let myString = "apple";
 
 // console.log(reverseCharacters(myString));
 
@@ -23,7 +23,7 @@ function reverseCharacters(str) {
 }
 let myString = "radar";
 
-console.log(reverseCharacters(myString));
+// console.log(reverseCharacters(myString));
 
 
 // Part Three: Complete Reversal
@@ -40,26 +40,34 @@ function reverseArray(array) {
     }
     return reverseArray;
 }
-console.log(reverseArray(arrayTest3));
+// console.log(reverseArray(arrayTest3));
 
 
 // Bonus Missions
 
-// 1. Have a clear, descriptive name like funPhrase.
-// 2. Retrieve only the last character from strings with lengths of 3 or less.
-// 3. Retrieve only the first 3 characters from strings with lengths larger than 3.
-// 4. Use a template literal to return the phrase We put the '___' in '___'. Fill the first blank with the modified string, and fill the second blank with the original string.
 
-// Test Function
+function funnyPhrase(str) {
+    if (str.length <= 3) {
+        return str[str.length - 1];
+    } else {
+        return str.slice(0, 3);
+    }
+}
+let funnyWord = "Functions rock!";
+// console.log(`We put the" '${funnyPhrase(funnyWord)}' in '${funnyWord}'.`);
 
-// 1. Outside of the function, define the variable str and initialize it with a string (e.g. 'Functions rock!').
-// 2. Call your function and print the returned phrase.
 
 // Area of rectangle equal to length x width
 
-// 1. Define a function with the required parameters to calculate the area of a rectangle.
-// 2. The function should return the area, NOT print it.
-// 3. Call your area function by passing in two arguments - the length and width.
-// 4. If only one argument is passed to the function, then the shape is a square. Modify your code to deal with this case.
-// 5. Use a template literal to print, “The area is ____ cm^2.”
 
+function calculateAreaOfRectangle(length, width) {
+    if (isNaN(width)) {
+        return length * length;
+    } else {
+        return length * width;
+    }
+}
+
+console.log(`The area is ${calculateAreaOfRectangle(2,4)} cm^2.`);
+console.log(`The area is ${calculateAreaOfRectangle(14,7)} cm^2.`);
+console.log(`The area is ${calculateAreaOfRectangle(20)} cm^2.`);
