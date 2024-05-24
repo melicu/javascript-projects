4dee3f0f47cfa6f8bf3f11f6d84697d899f84bbc
unknown <melcusumano@gmail.com>
2024-05-23 22:10:07 -0500

    Functions Studio Complete

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -2,33 +2,46 @@
 
 // Part One: Reverse Characters
 
-// 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
-// 2. Within the function, split the string into an array, then reverse the array.
-// 3. Use join to create the reversed string and return that string from the function.
-// 4. Below the function, define and initialize a variable to hold a string.
-// 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
-// 6. Optional: Use method chaining to reduce the lines of code within the function.
+
+// function reverseCharacters(str) {
+//     return str.split("").reverse().join("");
+// }
+// let myString = "apple";
+
+// console.log(reverseCharacters(myString));
+
 
 // Part Two: Reverse Digits
 
-// 1. Add an if statement to reverseCharacters to check the typeof the parameter.
-// 2. If typeof is ‘string’, return the reversed string as before.
-// 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
-// 4. Return the reversed number.
-// 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
+function reverseCharacters(str) {
+    if (typeof (str) === "string") {
+        return str.split("").reverse().join("");
+    } else {
+        let reverseString = String(str);
+        return Number(reverseString.split("").reverse().join(""));
+    }
+}
+let myString = "radar";
+
+console.log(reverseCharacters(myString));
+
 
 // Part Three: Complete Reversal
 
-// 1. Define and initialize an empty array.
-// 2. Loop through the old array.
-// 3. For each element in the old array, call reverseCharacters to flip the characters or digits.
-// 4. Add the reversed string (or number) to the array defined in part ‘a’.
-// 5. Return the final, reversed array.
-// 6. Be sure to print the results from each test case in order to verify your code.
 
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
+
+function reverseArray(array) {
+    let reverseArray = [];
+    for (index = 0; index < array.length; index++) {
+        reverseArray.unshift(reverseCharacters(array[index]));
+    }
+    return reverseArray;
+}
+console.log(reverseArray(arrayTest3));
+
 
 // Bonus Missions
 
@@ -49,3 +62,12 @@ let arrayTest3 = ['hello', 'world', 123, 'orange'];
 // 3. Call your area function by passing in two arguments - the length and width.
 // 4. If only one argument is passed to the function, then the shape is a square. Modify your code to deal with this case.
 // 5. Use a template literal to print, “The area is ____ cm^2.”
+
+
+
+// function reverseCharacters(stringToReverse) {
+//     if (typeofstringtoreverse === "string") {}
+//     if type of string to reverse === "number) {
+//         stringtoREverse = string(stringtoreverse);
+//         return Number(stringtoreverse.split(''"
+// }
