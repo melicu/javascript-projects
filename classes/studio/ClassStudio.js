723526b02da9abaa9da8731bc8500b889c9bf70d
unknown <melcusumano@gmail.com>
2024-07-11 23:12:32 -0500

    Class Studio Complete

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,9 +1,82 @@
-//Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
+class CrewCandidate {
+    constructor(name, mass, scores) {
+        this.name = name;
+        this.mass = mass;
+        this.scores = scores;
+    }
+    addScore(newScore) {
+        this.scores.push(newScore);
+    }
+    average() {
+        let sum = 0
+        let average = 0
+        for (let index = 0; index < this.scores.length; index++) {
+            sum += this.scores[index];
+        }
+        average = Math.round((sum / this.scores.length) * 10) / 10;
+        return average;
+    }
+    status() {
+        let testAverage = this.average();
+        if (testAverage >= 90) {
+            return "Accepted";
+        } else if (testAverage >= 80) {
+            return "Reserve";
+        } else if (testAverage >= 70) {
+            return "Probationary";
+        } else {
+            return "Rejected";
+        }
+    }
+    getAccepted() {
+        let numTests = 0
+        while (this.average() < 90) {
+            this.addScore(100);
+            numTests++;
+        }
+        return `${this.name} needs ${numTests} perfect test scores to reach 'Accepted' status.`;
+    }
+}
 
+let bear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
+let maltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+let gator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
 
+console.log(gator.average());
+console.log(gator.getAccepted());
+console.log(gator.status());
+console.log(gator.average());
 
-//Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
+console.log(maltese.getAccepted());
+console.log(bear.getAccepted());
 
+// console.log(bear);
+// console.log(maltese);
+// console.log(gator);
 
+// bear.addScore(83);
+// console.log(bear.scores);
 
-//Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
+// console.log(maltese.average());
+
+// console.log(`${bear.name} earned an average test score of ${bear.average()} and has a status of ${bear.status()}.`);
+// console.log(`${maltese.name} earned an average test score of ${maltese.average()} and has a status of ${maltese.status()}.`);
+// console.log(`${gator.name} earned an average test score of ${gator.average()} and has a status of ${gator.status()}.`);
+
+// gator.addScore(100);
+// console.log(gator.status());
+
+// gator.addScore(100);
+// console.log(gator.status());
+
+// gator.addScore(100);
+// console.log(gator.status());
+
+// gator.addScore(100);
+// console.log(gator.status());
+
+// gator.addScore(100);
+// console.log(gator.status());
+
+// gator.addScore(100);
+// console.log(gator.status());
