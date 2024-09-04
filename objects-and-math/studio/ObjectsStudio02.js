96b1f0ef86effa95150633b9d4c5c6a97ff625a1
unknown <melcusumano@gmail.com>
2024-09-03 21:52:05 -0500

    Class 7 Studio Complete

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,58 +1,84 @@
 // Code your orbitCircumference function here:
 
+function calculateOrbitCircumference(orbitRadius) {
+  return Math.round(2 * Math.PI * orbitRadius);
+}
 
 // Code your missionDuration function here:
 
+let orbitRadius = 2000;
+let orbitalSpeed = 28000;
+
+function missionDuration(numOrbits, orbitRadius, orbitalSpeed) {
+  return Math.round((numOrbits * ((calculateOrbitCircumference(orbitRadius)) / orbitalSpeed)) * 100) / 100;
+}
+
+console.log(`The mission will travel ${calculateOrbitCircumference(orbitRadius) * 5} km around the planet, and it will take ${missionDuration(5, orbitRadius, orbitalSpeed)} hours to complete.`);
 
 // Copy/paste your selectRandomEntry function here:
 
+function selectRandomEntry(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
 
 // Code your oxygenExpended function here:
+
+function oxygenExpended(candidate, orbitRadius, orbitalSpeed) {
+  let time = missionDuration(3, 1500, 15000);
+  let oxygenUsed = Math.round((candidate.o2Used(time)) * 1000) / 1000;
+
+  Math.round(candidate.o2Used(missionDuration(3, orbitRadius, orbitalSpeed)) * 1000) / 1000;
+  return `${candidate.name} will perform the spacewalk, which will last ${time} hours and require ${oxygenUsed} kg of oxygen.`
+}
+
 
 
 // Candidate data & crew array.
 let candidateA = {
-   'name':'Gordon Shumway',
-   'species':'alf',
-   'mass':90,
-   'o2Used':function(hrs){return 0.035*hrs},
-   'astronautID':414
- };
- let candidateB = {
-   'name':'Lassie',
-   'species':'dog',
-   'mass':19.1,
-   'o2Used':function(hrs){return 0.030*hrs},
-   'astronautID':503
- };
- let candidateC = {
-   'name':'Jonsey',
-   'species':'cat',
-   'mass':3.6,
-   'o2Used':function(hrs){return 0.022*hrs},
-   'astronautID':796
- };
- let candidateD = {
-   'name':'Paddington',
-   'species':'bear',
-   'mass':31.8,
-   'o2Used':function(hrs){return 0.047*hrs},
-   'astronautID':291
- };
- let candidateE = {
-   'name':'Pete',
-   'species':'tortoise',
-   'mass':417,
-   'o2Used':function(hrs){return 0.010*hrs},
-   'astronautID':599
- };
- let candidateF = {
-   'name':'Hugs',
-   'species':'ball python',
-   'mass':2.3,
-   'o2Used':function(hrs){return 0.018*hrs},
-   'astronautID':890
- };
- 
- let crew = [candidateA,candidateC,candidateE];
- 
+  'name': 'Gordon Shumway',
+  'species': 'alf',
+  'mass': 90,
+  'o2Used': function (hrs) { return 0.035 * hrs },
+  'astronautID': 414
+};
+let candidateB = {
+  'name': 'Lassie',
+  'species': 'dog',
+  'mass': 19.1,
+  'o2Used': function (hrs) { return 0.030 * hrs },
+  'astronautID': 503
+};
+let candidateC = {
+  'name': 'Jonsey',
+  'species': 'cat',
+  'mass': 3.6,
+  'o2Used': function (hrs) { return 0.022 * hrs },
+  'astronautID': 796
+};
+let candidateD = {
+  'name': 'Paddington',
+  'species': 'bear',
+  'mass': 31.8,
+  'o2Used': function (hrs) { return 0.047 * hrs },
+  'astronautID': 291
+};
+let candidateE = {
+  'name': 'Pete',
+  'species': 'tortoise',
+  'mass': 417,
+  'o2Used': function (hrs) { return 0.010 * hrs },
+  'astronautID': 599
+};
+let candidateF = {
+  'name': 'Hugs',
+  'species': 'ball python',
+  'mass': 2.3,
+  'o2Used': function (hrs) { return 0.018 * hrs },
+  'astronautID': 890
+};
+
+let crew = [candidateA, candidateC, candidateE];
+
+
+console.log(oxygenExpended(selectRandomEntry(crew)));
--- objects-and-math/studio/ObjectsStudio03.js
+++ objects-and-math/studio/ObjectsStudio03.js
@@ -1,8 +1,30 @@
 // Code your crewMass function here:
 
+let crewMass = (selectedCrew) => {
+  let totalCrewMass = 0;
+  for (let index = 0; index < selectedCrew.length; index++) {
+    totalCrewMass += (selectedCrew[index].mass);
+  }
+  return (totalCrewMass * 10) / 10;
+}
 
 // Code your fuelRequired function here:
 
+let fuelRequired = (rocketMass, crewMass, selectedCrew) => {
+  let totalMass = rocketMass + crewMass;
+  let fuelNeeded = totalMass * 9.5;
+  for (let index = 0; index < selectedCrew.length; index++) {
+    if (selectedCrew[index].species === "dog" || selectedCrew[index].species === "cat") {
+      fuelNeeded += 200;
+    } else {
+      fuelNeeded += 100;
+    }
+  }
+  return `The mission has a launch mass of ${totalMass} kg and requires ${Math.ceil(fuelNeeded)} kg of fuel.`;
+  
+  
+  // Math.ceil(fuelNeeded);
+}
 
 // The pre-selected crew is in the array at the end of this file.
 // Feel free to add, remove, or switch crew members as you see fit.
@@ -51,4 +73,5 @@ let candidateA = {
  };
  
  let crew = [candidateB,candidateD,candidateF];
- 
+
+ console.log(fuelRequired(75000, crewMass(crew), crew));
